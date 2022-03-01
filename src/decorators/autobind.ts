9f99920f81dfff419707739a57a7fadbd950439f
ysmirnov <ysmirnov@griddynamics.com>
2022-03-01 09:06:44 +0200

    split to different files

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,15 @@
+namespace App {
+  // Autobind
+  export function autobind(_: any, _1: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+  
+}
